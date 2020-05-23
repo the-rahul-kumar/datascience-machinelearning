a89629fa9a864b3ccbbd67af98481f1d9615cb21
RC <c.rohitash@gmail.com>
2020-05-23 19:16:22 +1000

    r plots

---
+++ week0/Rbasics/plots.r
@@ -0,0 +1,49 @@
+
+# Get the input values.
+input <- mtcars[,c('wt','mpg')]
+
+# Give the chart file a name.
+png(file = "scatterplot.png")
+
+# Plot the chart for cars with weight between 2.5 to 5 and mileage between 15 and 30.
+plot(x = input$wt,y = input$mpg,
+     xlab = "Weight",
+     ylab = "Milage",
+     xlim = c(2.5,5),
+     ylim = c(15,30),		 
+     main = "Weight vs Milage"
+)
+
+# Save the file.
+dev.off()
+
+input <- mtcars[,c('mpg','cyl')]
+print(head(input))
+
+# Give the chart file a name.
+png(file = "boxplot.png")
+
+# Plot the chart.
+boxplot(mpg ~ cyl, data = mtcars, xlab = "Number of Cylinders",
+        ylab = "Miles Per Gallon", main = "Mileage Data")
+
+# Save the file.
+dev.off()
+
+
+
+# Create the data for the chart.
+v <- c(7,12,28,3,41)
+t <- c(14,7,6,19,3)
+
+# Give the chart file a name.
+png(file = "lines.png")
+
+# Plot the bar chart.
+plot(v,type = "o",col = "red", xlab = "Month", ylab = "Rain fall", 
+     main = "Rain fall chart")
+
+lines(t, type = "o", col = "blue")
+
+# Save the file.
+dev.off()
