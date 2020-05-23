9e51187d260e906a38d3948d943200bd24bd6505
RC <c.rohitash@gmail.com>
2020-05-23 18:51:50 +1000

    examples updated

--- week0/Rbasics/data_input.r
+++ week0/Rbasics/data_input.r
@@ -1,6 +1,22 @@
+
+
+library(data.table)
+mydat <- fread('http://www.stats.ox.ac.uk/pub/datasets/csb/ch11b.dat')
+head(mydat)
+
+
+
+
+
+
+
+
+
 data <- read.csv("input.csv")
 print(data)
 
 print(is.data.frame(data))
 print(ncol(data))
 print(nrow(data))
+
+
